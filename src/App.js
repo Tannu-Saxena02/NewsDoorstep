bfda2898c760d5c4b5f2ca63f8167514fa1ae379
Tannu_Saxena <tannusaxena02@gmail.com>
2023-10-23 12:25:56 +0530

    route fixes.

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ const App =()=> {
         progress={progress}
       />
       <Routes>
-            <Route exact path="/" element={<News setProgress={setProgress} apiKey={apiKey} key={"business"} pageSize={pageSize} country={"in"} category={"business"}/>}/>
+            <Route exact path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key={"business"} pageSize={pageSize} country={"in"} category={"business"}/>}/>
             <Route exact path="/entertainment"element={<News setProgress={setProgress} apiKey={apiKey} key={"entertainment"}  pageSize={pageSize} country={"in"} category={"entertainment"}/>}/>
             <Route exact path="/general"element={<News setProgress={setProgress} apiKey={apiKey} key={"general"}  pageSize={pageSize} country={"in"} category={"general"}/>}/>
             <Route exact path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key={"health"} pageSize={pageSize} country={"in"} category={"health"}/>}/>
